d9c1b828d8122f2c13ab74be1d85195c8beca0b8
mukhammadqodir-alijonov <mukhammadqodir-alijonov.alijonov0608@gmail.com>
2023-07-03 19:56:22 +0500

    the project is started

---
+++ restauran.console/restauran.console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace restauran.console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
