ddcdf83a7ca57a4f337171d7978ff7926e3ef837
mukhammadkodir <98277057+mukhammadkodir-alijonov@users.noreply.github.com>
2023-07-03 22:47:04 +0500

    Update DbConstants.cs

--- restauran.console/restauran.console/Constants/DbConstants.cs
+++ restauran.console/restauran.console/Constants/DbConstants.cs
@@ -2,7 +2,7 @@
 {
     public class DbConstants
     {
-        public const string PRODUCTS_DB = "Database/products.json";
+        public const string PRODUCTS_DB = "Database/products.jon";
         public const string ORDERS_DB = "Database/orders.json";
         public const string EMPLOYEES_DB = "Database/employes.json";
         public const string ORDER_DETAILS_DB = "Database/order_details.json";
