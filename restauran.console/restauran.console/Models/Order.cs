7f4edd707835a81016e7de5900aa24dbee77994a
mukhammadkodir <98277057+mukhammadkodir-alijonov@users.noreply.github.com>
2023-07-05 15:17:00 +0500

    Update Order.cs

--- restauran.console/restauran.console/Models/Order.cs
+++ restauran.console/restauran.console/Models/Order.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int TableName { get; set; }
         public double TotalSum { get; set; }
-        public string OrderDate { get; set; }
+        public string OrderDate { get; set; } = string.Empty;
         public int MumberCount { get; set; }
         public int EmployeeId { get; set; }
         public bool isPaid { get; set; }
